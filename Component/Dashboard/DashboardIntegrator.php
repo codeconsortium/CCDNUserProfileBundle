2f93c4ea672fa03c774ece5011b821324e5af338
codeconsortium <reece@codeconsortium.com>
2012-03-20 04:05:29 +0000

    Add support for new DashboardBundle.

---
+++ Component/Dashboard/DashboardIntegrator.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the CCDNUser ProfileBundle
+ *
+ * (c) CCDN (c) CodeConsortium <http://www.codeconsortium.com/> 
+ * 
+ * Available on github <http://www.github.com/codeconsortium/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CCDNUser\ProfileBundle\Component\Dashboard;
+
+use CCDNComponent\DashboardBundle\Component\Integrator\BaseIntegrator;
+
+/**
+ * 
+ * @author Reece Fowell <reece@codeconsortium.com> 
+ * @version 1.0
+ */
+class DashboardIntegrator extends BaseIntegrator
+{
+
+	
+	
+	/**
+	 *
+	 *
+	 * Structure of $resources
+	 * 	[DASHBOARD_PAGE String]
+	 * 		[CATEGORY_NAME String]
+	 *			[ROUTE_FOR_LINK String]
+	 *				[AUTH String]
+	 *				[URL_LINK String]
+	 *				[URL_NAME String]
+	 */
+	public function getResources()
+	{
+		$resources = array(
+			'user' => array(
+				'Account' => array(
+					'cc_profile_show' => array('auth' => 'ROLE_USER', 'url' => $this->basePath . '/' . $this->locale . '/my/profile', 'name' => 'My Profile', 'icon' => $this->basePath . '/bundles/ccdncomponentcommon/images/icons/Black/32x32/32x32_user.png'),
+					'cc_profile_edit' => array('auth' => 'ROLE_USER', 'url' => $this->basePath . '/' . $this->locale . '/my/profile/edit', 'name' => 'Edit My Profile', 'icon' => $this->basePath . '/bundles/ccdncomponentcommon/images/icons/Black/32x32/32x32_user.png'),
+				),
+			),
+
+		);
+		
+		return $resources;
+	}
+	
+}
+
