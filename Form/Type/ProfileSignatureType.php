45dffd9e8beeb2df77ccab888711c3548074f9ee
Thomas Peterson <info@thomas-peterson.de>
2012-10-24 01:14:30 +0200

    Fix Symfony 2.1.3-dev Form

--- Form/Type/ProfileSignatureType.php
+++ Form/Type/ProfileSignatureType.php
@@ -14,7 +14,7 @@
 namespace CCDNUser\ProfileBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 
 /**
  *
@@ -29,7 +29,7 @@ class ProfileSignatureType extends AbstractType
      * @access public
      * @param FormBuilder $builder, array $options
      */
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('signature');
