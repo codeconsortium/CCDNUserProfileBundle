88b42091e24f44b98a9c988fa80f1184e40c9810
Reece Fowell <reece@codeconsortium.com>
2013-12-07 00:38:17 +0000

    swap doctrine registry for object manager for gateway param

--- Model/Gateway/BaseGateway.php
+++ Model/Gateway/BaseGateway.php
@@ -13,8 +13,9 @@
 
 namespace CCDNUser\ProfileBundle\Model\Gateway;
 
-use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\ORM\QueryBuilder;
+use Knp\Component\Pager\Paginator;
 
 /**
  *
@@ -33,21 +34,7 @@ abstract class BaseGateway
     /**
      *
      * @access protected
-     * @var \Doctrine\Bundle\DoctrineBundle\Registry $doctrine
-     */
-    protected $doctrine;
-
-    /**
-     *
-     * @access protected
-     * @var $paginator
-     */
-    protected $paginator;
-	
-    /**
-     *
-     * @access protected
-     * @var \Doctrine\ORM\EntityManager $em
+     * @var \Doctrine\Common\Persistence\ObjectManager $em
      */
     protected $em;
 
@@ -60,6 +47,13 @@ abstract class BaseGateway
 
     /**
      *
+     * @access protected
+     * @var $paginator
+     */
+    protected $paginator;
+
+    /**
+     *
      * @access private
      * @var string $pagerTheme
      */
@@ -68,21 +62,20 @@ abstract class BaseGateway
     /**
      *
      * @access public
-     * @param  \Doctrine\Bundle\DoctrineBundle\Registry                   $doctrine
-     * @param  string                                                     $entityClass
-     * @param  string                                                     $pagerTheme
+     * @param  \Doctrine\Common\Persistence\ObjectManager $em
+     * @param  string                                     $entityClass
+     * @param  \Knp\Component\Pager\Paginator             $paginator
+     * @param  string                                     $pagerTheme
      */
-    public function __construct(Registry $doctrine, $paginator, $entityClass, $pagerTheme)
+    public function __construct(ObjectManager $em, $entityClass, Paginator $paginator = null, $pagerTheme = null)
     {
-        $this->doctrine = $doctrine;
-        $this->em = $doctrine->getEntityManager();
-		$this->paginator = $paginator;
-
         if (null == $entityClass) {
             throw new \Exception('Entity class for gateway must be specified!');
         }
 
         $this->entityClass = $entityClass;
+        $this->em = $em;
+		$this->paginator = $paginator;
 		$this->pagerTheme = $pagerTheme;
     }
 
