c9b1e3ada19cf8723b1322b3aa77d79375be36e4
Reece Fowell <reece@codeconsortium.com>
2014-01-26 13:33:14 +0000

    move form theme, and paginator theme out of bundle, use symfony/paginator default themes

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -45,14 +45,14 @@ class Configuration implements ConfigurationInterface
      * @access protected
      * @var string $defaultValueFormTheme
      */
-    protected $defaultValueFormTheme = 'CCDNUserProfileBundle:Common:Form/fields.html.twig';
+	protected $defaultValueFormTheme = 'form_div_layout.html.twig';
 
     /**
      *
      * @access protected
      * @var string $defaultValuePaginatorTheme
      */
-    protected $defaultValuePaginatorTheme = 'CCDNUserProfileBundle:Common:Paginator/twitter_bootstrap.html.twig';
+	protected $defaultValuePaginatorTheme = null;
 
     /**
      *
